433e19da2812d39449a7e19c90baa3205380933e
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-05-12 18:35:19 -0400

    Adiciona incrementing como false

--- app/Models/Traits/Uuid.php
+++ app/Models/Traits/Uuid.php
@@ -10,6 +10,7 @@ trait Uuid
     {
         parent::boot();
         static::creating(function ($model) {
+            $model->incrementing = false;
             $model->{$model->getKeyName()} = Str::uuid()->toString();
         });
     }
