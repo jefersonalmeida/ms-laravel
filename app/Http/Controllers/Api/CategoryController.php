aa8a04a2b242c7ece0c9a6c9374d61ecee79b4cb
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-05-12 18:20:28 -0400

    Cria controller para category

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+
+class CategoryController extends Controller
+{
+    private array $rules = [
+        'name' => ['required', 'max:255'],
+        'is_active' => ['boolean'],
+        'description' => ['nullable']
+    ];
+
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->rules);
+        return Category::create($request->all());
+    }
+
+    public function show(Category $category): Category
+    {
+        return $category;
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function update(Request $request, Category $category): Category
+    {
+        $this->validate($request, $this->rules);
+        $category->update($request->all());
+        return $category;
+    }
+
+    public function destroy(Category $category): Response
+    {
+        $category->delete();
+        return response()->noContent();
+    }
+}
