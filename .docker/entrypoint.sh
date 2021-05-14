24a6cd8a7fafc9fbe498ff4fdd78e99a8a809b4f
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-05-14 19:11:33 -0400

    Finalizing module tests with HTTP

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
-chown -R www-data:www-data .
+if [ ! -f ".env" ]; then
+  cp .env.example .env
+fi
+if [ ! -f ".env.testing" ]; then
+  cp .env.testing.example .env.testing
+fi
 composer install
 php artisan key:generate
 php artisan migrate
