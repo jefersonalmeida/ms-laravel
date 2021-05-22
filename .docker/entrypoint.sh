ee1af183301357f235710b21414cc057b4819186
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-05-22 17:40:20 -0400

    Separating Laravel application to backend folder

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
-if [ ! -f ".env" ]; then
-    cp .env.example .env
-fi
-if [ ! -f ".env.testing" ]; then
-    cp .env.testing.example .env.testing
-fi
+##########################################
+# BACKEND INIT
+cd backend || exit
+cp -R .env.example .env
+cp -R .env.testing.example .env.testing
 composer install
 php artisan key:generate
 php artisan migrate
 chmod -R 777 ./storage/
 chmod -R 777 ./bootstrap/cache/
-
+cd ..
+##########################################
+# BACKEND END
 php-fpm
